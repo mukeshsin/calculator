fc9126b5cf514a8924e170f0fbc096c62d5e2c3d
mukeshsin <mukesh@innotechteam.in>
2022-09-01 12:59:06 +0530

    all changes done

--- calc.js
+++ calc.js
@@ -4,7 +4,8 @@ const calcResultE1 = document.querySelector(".calc-result");
 const numbersE1 = document.querySelectorAll(".number");
 const operationE1 = document.querySelectorAll(".operation");
 const equalE1 = document.querySelector(".equal");
-const clearbuttonE1 = document.querySelector(".btn-clear");
+const clearAllE1 = document.querySelector(".all-clear");
+
 
 let dis1Num = "";
 let dis2Num = "";
@@ -37,8 +38,11 @@ operationE1.forEach((operation) => {
         clearVar(operationName);
         lastOperation = operationName;
         console.log(result);
-    });
+    })
 });
+
+
+
 function clearVar(name = "") {
     dis1Num += dis2Num + "" + name + "";
     display1E1.innerText = dis1Num;
@@ -46,19 +50,9 @@ function clearVar(name = "") {
     dis2Num = "";
     calcResultE1.innerText = result;
 }
-clearbuttonE1.addEventListener("click", () => {
-    clearVar()
-})
-equalE1.addEventListener('click', () => {
-    if (!dis1Num || !dis2Num) return;
-    haveDot = false;
-    mathOperation();
-    clearVar();
-    display2E1.innerText = result;
-    calcResultE1.innerText = '';
-    dis2Num = result;
-    dis1Num = '';
-});
+
+
+
 
 function mathOperation() {
     if (lastOperation === "x") {
@@ -72,8 +66,21 @@ function mathOperation() {
     }
 
 }
-
-
-
-
-
+equalE1.addEventListener('click', (e) => {
+    if (!dis1Num || !dis2Num) return;
+    haveDot = false;
+    mathOperation();
+    clearVar();
+    display2E1.innerText = result;
+    calcResultE1.innerText = '';
+    dis2Num = result;
+    dis1Num = '';
+});
+clearAllE1.addEventListener("click", (e) => {
+    display1E1.innerText = '0';
+    display2E1.innerText = '0';
+    dis1Num.innerText = '';
+    dis2Num.innerText = '';
+    result = '';
+    calcResultE1.innerText = '0';
+});
